95c365069fd8cf9fb01fef45e946f3acb5d05823
Pavel Demin <pavel-demin@outlook.com>
2020-01-01 13:45:21 +0000

    set CIC decimation rate to 10 in sdr_receiver_wide

--- projects/sdr_receiver_wide/filters/fir_0.r
+++ projects/sdr_receiver_wide/filters/fir_0.r
@@ -1,7 +1,7 @@
 library(signal)
 
 # CIC filter parameters
-R <- 8                         # Decimation factor
+R <- 10                        # Decimation factor
 M <- 1                         # Differential delay
 N <- 6                         # Number of stages
 
