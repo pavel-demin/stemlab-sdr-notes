2f659f1ccf29e97bc8daa624b4d929505552b924
Pavel Demin <pavel-demin@outlook.com>
2020-01-08 20:33:42 +0000

    simplify sdr-receiver-wide.c

--- projects/sdr_receiver_wide/server/sdr-receiver-wide.c
+++ projects/sdr_receiver_wide/server/sdr-receiver-wide.c
@@ -23,13 +23,12 @@ void signal_handler(int sig)
 
 int main ()
 {
-  pid_t pid;
-  int pipefd[2], mmapfd, sockServer, sockClient;
+  int mmapfd, sockServer, sockClient;
   int position, limit, offset;
   volatile uint32_t *slcr, *axi_hp0;
   volatile void *cfg, *sts, *ram, *buf;
   struct sockaddr_in addr;
-  int yes = 1, buffer = 0;
+  int yes = 1;
 
   if((mmapfd = open("/dev/mem", O_RDWR)) < 0)
   {
@@ -52,17 +51,51 @@ int main ()
 
   limit = 512*1024;
 
-  /* create a pipe */
-  pipe(pipefd);
-
-  pid = fork();
-  if(pid == 0)
+  if((sockServer = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
-    /* child process */
+    perror("socket");
+    return 1;
+  }
 
-    close(pipefd[0]);
+  setsockopt(sockServer, SOL_SOCKET, SO_REUSEADDR, (void *)&yes, sizeof(yes));
 
-    while(1)
+  /* setup listening address */
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(TCP_PORT);
+
+  if(bind(sockServer, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+  {
+    perror("bind");
+    return 1;
+  }
+
+  listen(sockServer, 1024);
+
+  while(!interrupted)
+  {
+    /* enter reset mode */
+    *(uint8_t *)(cfg + 0) &= ~1;
+    *(uint8_t *)(cfg + 0) &= ~2;
+    /* set CIC decimation rate */
+    *(uint16_t *)(cfg + 2) = 10;
+    /* set frequencies */
+    *(uint32_t *)(cfg + 4) = (uint32_t)floor(10000000 / 122.88e6 * (1<<30) + 0.5);
+    *(uint32_t *)(cfg + 8) = (uint32_t)floor(10000000 / 122.88e6 * (1<<30) + 0.5);
+
+    if((sockClient = accept(sockServer, NULL, NULL)) < 0)
+    {
+      perror("accept");
+      return 1;
+    }
+
+    signal(SIGINT, signal_handler);
+
+    /* enter normal operating mode */
+    *(uint8_t *)(cfg + 0) |= 3;
+
+    while(!interrupted)
     {
       /* read ram writer position */
       position = *(uint32_t *)(sts + 12);
@@ -73,85 +106,23 @@ int main ()
         offset = limit > 0 ? 0 : 4096*1024;
         limit = limit > 0 ? 0 : 512*1024;
         memcpy(buf + offset, ram + offset, 4096*1024);
-        write(pipefd[1], &buffer, sizeof(buffer));
+        if(send(sockClient, buf + offset, 4096*1024, MSG_NOSIGNAL) < 0) break;
       }
       else
       {
         usleep(1000);
       }
     }
+
+    signal(SIGINT, SIG_DFL);
+    close(sockClient);
   }
-  else if(pid > 0)
-  {
-    /* parent process */
 
-    close(pipefd[1]);
+  /* enter reset mode */
+  *(uint8_t *)(cfg + 0) &= ~1;
+  *(uint8_t *)(cfg + 0) &= ~2;
 
-    if((sockServer = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-      perror("socket");
-      return 1;
-    }
+  close(sockServer);
 
-    setsockopt(sockServer, SOL_SOCKET, SO_REUSEADDR, (void *)&yes , sizeof(yes));
-
-    /* setup listening address */
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(TCP_PORT);
-
-    if(bind(sockServer, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-    {
-      perror("bind");
-      return 1;
-    }
-
-    listen(sockServer, 1024);
-
-    while(!interrupted)
-    {
-      /* enter reset mode */
-      *(uint8_t *)(cfg + 0) &= ~1;
-      *(uint8_t *)(cfg + 0) &= ~2;
-      /* set CIC decimation rate */
-      *(uint16_t *)(cfg + 2) = 10;
-      /* set frequencies */
-      *(uint32_t *)(cfg + 4) = (uint32_t)floor(600000 / 122.88e6 * (1<<30) + 0.5);
-      *(uint32_t *)(cfg + 8) = (uint32_t)floor(600000 / 122.88e6 * (1<<30) + 0.5);
-
-      if((sockClient = accept(sockServer, NULL, NULL)) < 0)
-      {
-        perror("accept");
-        return 1;
-      }
-
-      signal(SIGINT, signal_handler);
-
-      /* enter normal operating mode */
-      *(uint8_t *)(cfg + 0) |= 3;
-
-      while(!interrupted)
-      {
-        read(pipefd[0], &buffer, sizeof(buffer));
-        if(send(sockClient, buf, 4096*1024, 0) < 0) break;
-
-        read(pipefd[0], &buffer, sizeof(buffer));
-        if(send(sockClient, buf + 4096*1024, 4096*1024, 0) < 0) break;
-      }
-
-      signal(SIGINT, SIG_DFL);
-      close(sockClient);
-    }
-
-    /* enter reset mode */
-    *(uint8_t *)(cfg + 0) &= ~1;
-    *(uint8_t *)(cfg + 0) &= ~2;
-
-    close(sockServer);
-
-    kill(pid, SIGTERM);
-
-    return 0;
-  }
+  return 0;
 }
