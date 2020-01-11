f40ea6e0363f7d83ca2b460a4758a5bde8c94eb6
Pavel Demin <pavel-demin@outlook.com>
2020-01-11 21:48:05 +0000

    add commands to sdr-receiver-wide.c

--- projects/sdr_receiver_wide/server/sdr-receiver-wide.c
+++ projects/sdr_receiver_wide/server/sdr-receiver-wide.c
@@ -28,6 +28,8 @@ int main ()
   volatile uint32_t *slcr, *axi_hp0;
   volatile void *cfg, *sts, *ram, *buf;
   struct sockaddr_in addr;
+  uint32_t command, size;
+  int32_t value;
   int yes = 1;
 
   if((mmapfd = open("/dev/mem", O_RDWR)) < 0)
@@ -48,8 +50,6 @@ int main ()
   slcr[144] = 0;
   axi_hp0[0] &= ~1;
   axi_hp0[5] &= ~1;
-
-  limit = 512*1024;
 
   if((sockServer = socket(AF_INET, SOCK_STREAM, 0)) < 0)
   {
@@ -77,10 +77,11 @@ int main ()
   {
     /* enter reset mode */
     *(uint8_t *)(cfg + 0) &= ~1;
+    usleep(100);
     *(uint8_t *)(cfg + 0) &= ~2;
-    /* set CIC decimation rate */
+    /* set default sample rate */
     *(uint16_t *)(cfg + 2) = 10;
-    /* set frequencies */
+    /* set default phase increments */
     *(uint32_t *)(cfg + 4) = (uint32_t)floor(10000000 / 122.88e6 * (1<<30) + 0.5);
     *(uint32_t *)(cfg + 8) = (uint32_t)floor(10000000 / 122.88e6 * (1<<30) + 0.5);
 
@@ -95,8 +96,35 @@ int main ()
     /* enter normal operating mode */
     *(uint8_t *)(cfg + 0) |= 3;
 
+    limit = 512*1024;
+
     while(!interrupted)
     {
+      if(ioctl(sockClient, FIONREAD, &size) < 0) break;
+
+      if(size >= 4)
+      {
+        if(recv(sockClient, (char *)&command, 4, MSG_WAITALL) < 0) break;
+        value = command & 0xfffffff;
+        switch(command >> 28)
+        {
+          case 0:
+            /* set sample rate */
+            if(value < 8 || value > 64) continue;
+            *(uint16_t *)(cfg + 2) = value;
+          case 1:
+            /* set first phase increment */
+            if(value < 0 || value > 61440000) continue;
+            *((uint32_t *)(cfg + 4)) = (uint32_t)floor(value / 122.88e6 * (1<<30) + 0.5);
+            break;
+          case 2:
+            /* set first phase increment */
+            if(value < 0 || value > 61440000) continue;
+            *((uint32_t *)(cfg + 8)) = (uint32_t)floor(value / 122.88e6 * (1<<30) + 0.5);
+            break;
+        }
+      }
+
       /* read ram writer position */
       position = *(uint32_t *)(sts + 12);
 
@@ -120,6 +148,7 @@ int main ()
 
   /* enter reset mode */
   *(uint8_t *)(cfg + 0) &= ~1;
+  usleep(100);
   *(uint8_t *)(cfg + 0) &= ~2;
 
   close(sockServer);
